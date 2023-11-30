718389dca708bcf66084937263c6c0fd053b7931
Andrei Dayrit <22058759@saultcollege.ca>
2023-11-30 12:32:19 -0500

    added lab4
    Added Lab 4

---
+++ src/main/java/lab4/DiscMagazine.java
@@ -0,0 +1,25 @@
+package lab4;
+
+import lab3.q3.Publication;
+
+public class DiscMagazine extends Publication {
+    private int numberOfDiscs;
+
+    public DiscMagazine(String title, double price, int quantityInStock, int numberOfDiscs) {
+        super(title, price, quantityInStock);
+        this.numberOfDiscs = numberOfDiscs;
+    }
+
+    public int getNumberOfDiscs() {
+        return numberOfDiscs;
+    }
+
+    public void setNumberOfDiscs(int numberOfDiscs) {
+        this.numberOfDiscs = numberOfDiscs;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Number of Discs: " + numberOfDiscs;
+    }
+}
