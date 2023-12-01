31b92ebf54539fb102562f978189fa040c38b622
Andrei Dayrit <22058759@saultcollege.ca>
2023-12-01 15:15:12 -0500

    Added Pencil and Ticket Class

---
+++ src/main/java/lab4/Pencil.java
@@ -0,0 +1,13 @@
+package lab4;
+
+public class Pencil {
+    private String color;
+
+    public Pencil(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
---
+++ src/main/java/lab4/Ticket.java
@@ -0,0 +1,13 @@
+package lab4;
+
+public class Ticket {
+    private String ticketNumber;
+
+    public Ticket(String ticketNumber) {
+        this.ticketNumber = ticketNumber;
+    }
+
+    public String getTicketNumber() {
+        return ticketNumber;
+    }
+}
